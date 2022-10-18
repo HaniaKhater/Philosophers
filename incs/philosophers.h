a3a06638d769c8e10958a4bb0e535c87170bc464
Hania hesham KHATER <hkhater@e2r7p16.clusters.42paris.fr>
2022-10-18 13:36:46 +0200

    first files

---
+++ incs/philosophers.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkhater <hkhater@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 06:31:09 by hkhater           #+#    #+#             */
+/*   Updated: 2022/10/16 06:51:01 by hkhater          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct	s_simulation
+{
+	int	nb;
+	int	die;
+	int	eat;
+	int	sleep;
+	int	meals;
+}	t_simulation;
+
+void	ft_putstr(char *str);
+void	print_update(int nb);
+
+#endif
